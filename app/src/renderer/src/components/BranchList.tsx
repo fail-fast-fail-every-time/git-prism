183a5de06ea3cfdfe51af2dcc1a87d5511e74013
Morten Bested <mortenbested@gmal.com>
2025-11-16 13:41:20 +0100

    Improved branch list in switch dialog etc. Now uses the command list component which is much prettier than the custom table that was used before.

--- app/src/renderer/src/components/BranchList.tsx
+++ app/src/renderer/src/components/BranchList.tsx
@@ -1,7 +1,6 @@
-import { GitBranch } from 'lucide-react'
-import { ReactElement, useState } from 'react'
-import { Input } from './shadcn/Input'
-import { Table, TableBody, TableCell, TableRow } from './shadcn/Table'
+import { GitBranchIcon } from 'lucide-react'
+import { ReactElement } from 'react'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './shadcn/command'
 
 interface BranchListProps {
   branches: string[]
@@ -10,48 +9,48 @@ interface BranchListProps {
 }
 
 export function BranchList({ branches, onSelectBranch, recentBranches }: BranchListProps): ReactElement {
-  const [filter, setFilter] = useState<string>('')
-  const branchesFiltered = branches.filter((b) => b?.includes(filter))
-  const localBranches = branchesFiltered.filter((b) => !b.startsWith('origin/'))
-  const remoteBranches = branchesFiltered.filter((b) => b.startsWith('origin/'))
+  const localBranches = branches.filter((b) => !b.startsWith('origin/'))
+  const localBranchesWithRecent = localBranches.filter((b) => !recentBranches?.includes(b))
+  const remoteBranches = branches.filter((b) => b.startsWith('origin/'))
 
   return (
-    <div className="flex flex-col gap-2">
-      <Input placeholder="Filter branches" value={filter} onChange={(e) => setFilter(e.target.value)} />
-      <div className="h-[400px] overflow-y-auto">
-        <Table className="bg-secondary/50 mt-0 rounded-lg">
-          <TableBody>
-            {recentBranches && recentBranches.length > 0 && (
-              <TableRow>
-                <TableCell className="p-3 font-semibold">Recent branches</TableCell>
-              </TableRow>
-            )}
-            {recentBranches?.map((branch) => branchRow(branch, () => onSelectBranch(branch)))}
-            {localBranches.length > 0 && (
-              <TableRow>
-                <TableCell className="p-3 font-semibold">Local branches</TableCell>
-              </TableRow>
-            )}
-            {localBranches.map((branch) => branchRow(branch, () => onSelectBranch(branch)))}
-            {remoteBranches.length > 0 && (
-              <TableRow>
-                <TableCell className="p-3 font-semibold">Remote branches</TableCell>
-              </TableRow>
-            )}
-            {remoteBranches.map((branch) => branchRow(branch, () => onSelectBranch(branch)))}
-          </TableBody>
-        </Table>
-      </div>
+    <div className="flex flex-col gap-2 h-[400px] overflow-y-auto">
+      <Command>
+        <CommandInput placeholder="Search branch..." />
+        <CommandList>
+          <CommandEmpty>No branches found.</CommandEmpty>
+          {recentBranches && recentBranches.length > 0 && (
+            <CommandGroup heading="Recent branches">
+              {recentBranches.map((branch: string) => (
+                <BranchCommandItem key={'recent-' + branch} branch={branch} onSelect={() => onSelectBranch(branch)} />
+              ))}
+            </CommandGroup>
+          )}
+          {localBranchesWithRecent.length > 0 && (
+            <CommandGroup heading="Other branches">
+              {localBranchesWithRecent.map((branch: string) => (
+                <BranchCommandItem key={'local' + branch} branch={branch} onSelect={() => onSelectBranch(branch)} />
+              ))}
+            </CommandGroup>
+          )}
+          {remoteBranches.length > 0 && (
+            <CommandGroup heading="Remote branches">
+              {remoteBranches.map((branch: string) => (
+                <BranchCommandItem key={branch} branch={branch} onSelect={() => onSelectBranch(branch)} />
+              ))}
+            </CommandGroup>
+          )}
+        </CommandList>
+      </Command>
     </div>
   )
 }
 
-function branchRow(branch: string, onClick: () => void): ReactElement {
+function BranchCommandItem({ branch, onSelect }: { branch: string; onSelect: () => void }): ReactElement {
   return (
-    <TableRow key={branch} className="cursor-pointer" onClick={onClick}>
-      <TableCell className="flex gap-2 py-3">
-        <GitBranch size={14} className="mt-1" /> {branch}
-      </TableCell>
-    </TableRow>
+    <CommandItem value={branch} onSelect={onSelect} className="group flex cursor-pointer">
+      <GitBranchIcon className="mr-2 w-4 h-4" />
+      <div className="flex-1">{branch}</div>
+    </CommandItem>
   )
 }
